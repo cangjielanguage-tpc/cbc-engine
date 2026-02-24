f1cfb38d72cd33e20d239db275a53c75e87f5978
Abdurakhmanov Artem <abdurakhmanov.artem@huawei.com>
2026-02-24 14:16:17 +0700

    feat: Emitter using namespace

--- src/cbc/emitter/emitter_memspace.cpp
+++ src/cbc/emitter/emitter_memspace.cpp
@@ -9,9 +9,7 @@
 namespace Cbc {
 namespace Emitter {
 
-using Width  = Format::Width;
-using CC     = Format::CC;
-using Common = Format::Common;
+using namespace Format;
 
 using MemSpaceEmitter = Emitter::MemSpace;
 
@@ -57,28 +55,28 @@ void MemSpaceEmitter::OffsetReg(IReg reg)
         segment,
         RT::M2xr {
             .opc = RT::MemOpcode::OFFS_REG,
-            .xr  = Format::XR { .imm = 0, .r = reg },
+            .xr  = XR { .imm = 0, .r = reg },
         }
     );
 }
 
-static RT::MemOpcode ComputeLoadAccessKind(Format::LoadAccessKind ldk, RT::MemOpcode start)
+static RT::MemOpcode ComputeLoadAccessKind(LoadAccessKind ldk, RT::MemOpcode start)
 {
     // This code is heavily rely on the fact that opcodes are ordered
     // in the same order as in the switch here.
     uint8_t delta = 0;
     switch (ldk) {
-        case Format::LoadAccessKind::LD_U8:      delta = 0; break;
-        case Format::LoadAccessKind::LD_U16:     delta = 1; break;
-        case Format::LoadAccessKind::LD_32:      delta = 2; break;
-        case Format::LoadAccessKind::LD_S8:      delta = 3; break;
-        case Format::LoadAccessKind::LD_S16:     delta = 4; break;
-        case Format::LoadAccessKind::LD_F32:     delta = 5; break;
-        case Format::LoadAccessKind::LD_F64:     delta = 6; break;
-        case Format::LoadAccessKind::LD_64:      delta = 7; break;
-        case Format::LoadAccessKind::LD_S32TO64: delta = 8; break;
-        case Format::LoadAccessKind::LD_REF:     delta = 9; break;
-        default:                                 ASSERTION(false, "unexpected ldk");
+        case LoadAccessKind::LD_U8:      delta = 0; break;
+        case LoadAccessKind::LD_U16:     delta = 1; break;
+        case LoadAccessKind::LD_32:      delta = 2; break;
+        case LoadAccessKind::LD_S8:      delta = 3; break;
+        case LoadAccessKind::LD_S16:     delta = 4; break;
+        case LoadAccessKind::LD_F32:     delta = 5; break;
+        case LoadAccessKind::LD_F64:     delta = 6; break;
+        case LoadAccessKind::LD_64:      delta = 7; break;
+        case LoadAccessKind::LD_S32TO64: delta = 8; break;
+        case LoadAccessKind::LD_REF:     delta = 9; break;
+        default:                         ASSERTION(false, "unexpected ldk");
     }
     return RT::MemOpcode(start + delta);
 }
@@ -89,54 +87,54 @@ static RT::MemOpcode ComputeStoreAccessKind(Format::StoreAccessKind stk, RT::Mem
     // in the same order as in the switch here.
     uint8_t delta = 0;
     switch (stk) {
-        case Format::StoreAccessKind::ST_8:   delta = 0; break;
-        case Format::StoreAccessKind::ST_16:  delta = 1; break;
-        case Format::StoreAccessKind::ST_32:  delta = 2; break;
-        case Format::StoreAccessKind::ST_64:  delta = 3; break;
-        case Format::StoreAccessKind::ST_REF: delta = 4; break;
-        case Format::StoreAccessKind::ST_F32: delta = 5; break;
-        case Format::StoreAccessKind::ST_F64: delta = 6; break;
-        default:                              ASSERTION(false, "unexpected ldk");
+        case StoreAccessKind::ST_8:   delta = 0; break;
+        case StoreAccessKind::ST_16:  delta = 1; break;
+        case StoreAccessKind::ST_32:  delta = 2; break;
+        case StoreAccessKind::ST_64:  delta = 3; break;
+        case StoreAccessKind::ST_REF: delta = 4; break;
+        case StoreAccessKind::ST_F32: delta = 5; break;
+        case StoreAccessKind::ST_F64: delta = 6; break;
+        default:                      ASSERTION(false, "unexpected ldk");
     }
     return RT::MemOpcode(start + delta);
 }
 
-void MemSpaceEmitter::LoadObj(Format::LoadAccessKind ldk, Format::Reg dst, IReg base)
+void MemSpaceEmitter::LoadObj(LoadAccessKind ldk, Reg dst, IReg base)
 {
     RT::MemOpcode opc = ComputeLoadAccessKind(ldk, RT::MemOpcode::RLD_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::RLD_END_OPCODE);
     LoadStore(ldk, dst, base, opc);
 }
 
-void MemSpaceEmitter::StoreObj(Format::StoreAccessKind stk, Format::Reg src, IReg base)
+void MemSpaceEmitter::StoreObj(StoreAccessKind stk, Reg src, IReg base)
 {
     RT::MemOpcode opc = ComputeStoreAccessKind(stk, RT::MemOpcode::RST_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::RST_END_OPCODE);
     LoadStore(stk, src, base, opc);
 }
 
-void MemSpaceEmitter::LoadRec(Format::LoadAccessKind ldk, Format::Reg dst, IReg base)
+void MemSpaceEmitter::LoadRec(LoadAccessKind ldk, Reg dst, IReg base)
 {
     RT::MemOpcode opc = ComputeLoadAccessKind(ldk, RT::MemOpcode::SLD_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::SLD_END_OPCODE);
     LoadStore(ldk, dst, base, opc);
 }
 
-void MemSpaceEmitter::StoreRec(Format::StoreAccessKind stk, Format::Reg src, IReg base)
+void MemSpaceEmitter::StoreRec(StoreAccessKind stk, Reg src, IReg base)
 {
     RT::MemOpcode opc = ComputeStoreAccessKind(stk, RT::MemOpcode::SST_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::SST_END_OPCODE);
     LoadStore(stk, src, base, opc);
 }
 
-void MemSpaceEmitter::LoadFrame(Format::LoadAccessKind ldk, Format::Reg dst)
+void MemSpaceEmitter::LoadFrame(LoadAccessKind ldk, Reg dst)
 {
     RT::MemOpcode opc = ComputeLoadAccessKind(ldk, RT::MemOpcode::FLD_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::FLD_END_OPCODE);
     LoadStore(ldk, dst, IReg::IRZ, opc);
 }
 
-void MemSpaceEmitter::StoreFrame(Format::StoreAccessKind stk, Format::Reg src)
+void MemSpaceEmitter::StoreFrame(StoreAccessKind stk, Reg src)
 {
     RT::MemOpcode opc = ComputeStoreAccessKind(stk, RT::MemOpcode::FST_START_OPCODE);
     ASSERT(opc <= RT::MemOpcode::FST_END_OPCODE);
