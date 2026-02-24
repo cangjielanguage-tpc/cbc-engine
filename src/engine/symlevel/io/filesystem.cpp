c98d57a1f748be36805bc4a274d86ef452aa5e23
wellox <zapanov.rinchin@huawei.com>
2026-02-24 11:15:17 +0000

    fix: removed stream-based file opening

--- src/engine/symlevel/io/filesystem.cpp
+++ src/engine/symlevel/io/filesystem.cpp
@@ -1,32 +1,37 @@
 #include "filesystem.h"
 #include "byte_array_random_access_file.h"
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 namespace IO {
 
 std::unique_ptr<RandomAccessFile> OpenFile(std::filesystem::path path)
 {
-    FILE* file = fopen(path.c_str(), "rb");
-    if (!file) {
+    // FIXME: remove linux specific code
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd == -1) {
         throw std::runtime_error("cannot open file " + path.string());
     }
 
-    fseek(file, 0, SEEK_END);
-    long rawLength = ftell(file);
+    struct stat stat;
+
+    int status     = fstat(fd, &stat);
+    auto rawLength = stat.st_size; // TODO: not working with symbolic links
     if (rawLength <= 0) {
-        fclose(file);
+        close(fd);
         throw std::runtime_error("empty file " + path.string());
     }
-
-    rewind(file);
 
     size_t fileLength = static_cast<size_t>(rawLength);
 
     char* data       = new char[fileLength];
-    size_t bytesRead = fread(data, 1, fileLength, file);
+    ssize_t n        = read(fd, data, fileLength);
 
-    fclose(file);
+    close(fd);
 
-    if (bytesRead != fileLength) {
+    if (n != fileLength) {
         delete[] data;
         throw std::runtime_error("read error " + path.string());
     }
