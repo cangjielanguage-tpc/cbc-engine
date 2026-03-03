dd804e25d31bbfec34d828164a001175fb090f51
wellox <zapanov.rinchin@huawei.com>
2026-03-03 07:51:12 +0000

    fix: remove strict warning check to allow gcc to work with bitfields

--- src/api/type.h
+++ src/api/type.h
@@ -129,12 +129,12 @@ public:
 
     inline constexpr bool Is(TypeKind kind) const { return GetTypeKind() == kind; }
 
-    inline constexpr bool Is(TypeFlag flag) const { return flagsRaw & (1 << static_cast<TypeFlag::Value>(flag)); }
+    inline constexpr bool Is(TypeFlag flag) const { return flagsRaw & (1u << static_cast<TypeFlag::Value>(flag)); }
 
     inline constexpr TypeFlags Or(TypeFlag flag) const
     {
         TypeFlags copy  = *this;
-        copy.flagsRaw  |= 1 << flag;
+        copy.flagsRaw   |= 1u << flag;
         return copy;
     }
 
