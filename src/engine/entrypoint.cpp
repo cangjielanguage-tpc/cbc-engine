5a0792f2bf6967b1c7c8c0bb9059f1b3f6b0cc18
wellox <zapanov.rinchin@huawei.com>
2026-03-07 06:08:58 +0000

    feat: hacky entrypoint for launcher

--- src/engine/entrypoint.cpp
+++ src/engine/entrypoint.cpp
@@ -1,14 +1,34 @@
+#include <filesystem>
+
 #include "cbc_engine.h"
 #include "engine/engine.h"
+#include "engine/symlevel/io/filesystem.h"
 #include "interpreter/function_handle.h"
 
-extern "C" {
 
+static std::mutex g_InitializationGuard;
+static bool g_Initialized;
 static char const* g_cbcPath;
 static char const* g_mainCbc;
 
+// TODO: init runtime interface
+void EnsureEngineInitialized() {
+    std::lock_guard guard(g_InitializationGuard);
+    if (g_Initialized) {
+        return;
+    }
+
+    Engine::Loader loader;
+    loader.Load(IO::OpenFile(std::filesystem::path(g_mainCbc)), g_mainCbc);
+    loader.Build();
+    g_Initialized = true;
+}
+
+extern "C" {
+
 CBC_EXPORT void* engine_get_entrypoint_trampoline(void)
 {
+    EnsureEngineInitialized();
     auto& engine = Engine::GetEngineInstance();
 
     Engine::Session session(engine);
