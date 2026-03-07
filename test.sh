5109ca5c6ca6c906ad27d58c903ce348ea924085
wellox <zapanov.rinchin@huawei.com>
2026-03-07 08:14:06 +0000

    fix: linker options for security requirements

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ mkdir -p "$BUILD_DIR"
 
 # Configure and build
 cd "$BUILD_DIR"
-cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DUNIT_TEST_MODE=1
+cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make -j$(nproc)
 
 # Run tests
