8fb4aa962274e263b2f8faed8f9e51718e4c03a6
Abdurakhmanov Artem <abdurakhmanov.artem@huawei.com>
2026-03-10 12:19:46 +0700

    fix: update simple.asm to format

--- test/resources/simple.asm
+++ test/resources/simple.asm
@@ -1,7 +1,5 @@
-@class default
-
-  @method main ()V
-
+@type default {
+  @method default.main()V {
     mov.64 IR3, 0x1
     mov.64 IR1, 0x0
     mov.64 IR2, 0x7
@@ -9,7 +7,5 @@ l:  add.64 IR1, IR1, IR2
     sub.64 IR2, IR2, IR3
     branch.if NE, IR2, IRZ, l
     ret.64 IR1
-
-  @methodend
-
-@classend
+  }
+}
