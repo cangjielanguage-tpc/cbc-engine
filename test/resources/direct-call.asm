ac400682167376a4c1293bd6f941368925d82b73
wellox <zapanov.rinchin@huawei.com>
2026-03-11 07:13:12 +0000

    fix: use accumalator for direct call test

--- test/resources/direct-call.asm
+++ test/resources/direct-call.asm
@@ -1,17 +1,18 @@
 @type default {
   @method default.main()Void {
     mov.64 IR1, 0x7
-    call.direct Method(default.foo(I64)I64), IR1
+    mov.64 IR2, 0x0
+    call.direct Method(default.foo(I64.I64)I64), IR1
     ret.64 IR1
   }
 
-  @method default.foo(I64)I64 {
+  @method default.foo(I64.I64)I64 {
     branch.if EQ, IR1, IRZ, r
-    mov.64 IR9, IR1
+    add.64 IR2, IR2, IR1
     mov.64 IR3, 0x1
     sub.64 IR1, IR1, IR3
-    call.direct Method(default.foo(I64)I64), IR1
-    add.64 IR1, IR1, IR9
-r:  ret.64 IR1
+    call.direct Method(default.foo(I64.I64)I64), IR1
+r:  mov.64 IR1, IR2
+    ret.64 IR1
   }
 }
