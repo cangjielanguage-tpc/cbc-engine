6f7f954c176c84472784ca8ff5f0459dede8612b
wellox <zapanov.rinchin@huawei.com>
2026-03-17 13:24:12 +0000

    feat: support for runtime

--- src/engine/entrypoint.cpp
+++ src/engine/entrypoint.cpp
@@ -1,15 +1,40 @@
 #include <filesystem>
 
+#include "RTInterface.h"
 #include "cbc_engine.h"
 #include "engine/engine.h"
 #include "engine/symlevel/io/filesystem.h"
+#include "interpreter/ectype.h"
 #include "interpreter/function_handle.h"
 
-
+static cjnative_interface_t cjnative_interface_instance;
 static std::mutex g_InitializationGuard;
 static bool g_Initialized;
 static char const* g_cbcPath;
 static char const* g_mainCbc;
+
+extern "C" {
+/// Exported symbols from the cbc engine shared library.
+CBC_EXPORT void* engine_get_entrypoint_trampoline(void);
+CBC_EXPORT void engine_set_cbcpath(char const* cbcPath);
+CBC_EXPORT void engine_set_main_cbc(char const* mainCbc);
+CBC_EXPORT void engine_bridge_init(
+    size_t size,
+    char const** options,
+    struct interpreter_interface_t* interpInterf,
+    struct cjnative_interface_t* rtInterf
+);
+CBC_EXPORT void interpreter_bridge_init(
+    size_t size,
+    char const** options,
+    struct interpreter_interface_t* interpInterf,
+    struct cjnative_interface_t* rtInterf
+);
+
+/// Internal engine symbols.
+void engine_c2i_call_pc_start();
+void engine_c2i_call_pc_end();
+} // extern "C"
 
 // TODO: init runtime interface
 void EnsureEngineInitialized() {
@@ -22,6 +47,16 @@ void EnsureEngineInitialized() {
     loader.Load(IO::OpenFile(std::filesystem::path(g_mainCbc)), g_mainCbc);
     loader.Build();
     g_Initialized = true;
+}
+
+static void FiberStart(fiber_specific_data_t* data)
+{
+    // FIXME: remove from interface?
+}
+
+static void FiberDestroy(fiber_specific_data_t* data)
+{
+    // FIXME: ectype cleanup
 }
 
 extern "C" {
@@ -46,5 +81,35 @@ CBC_EXPORT void* engine_get_entrypoint_trampoline(void)
 CBC_EXPORT void engine_set_cbcpath(char const* cbcPath) { g_cbcPath = cbcPath; }
 
 CBC_EXPORT void engine_set_main_cbc(char const* mainCbc) { g_mainCbc = mainCbc; }
+
+CBC_EXPORT void engine_runtime_bridge_initialize(
+    size_t size,
+    char const** options,
+    struct interpreter_interface_t* interpInterf,
+    struct cjnative_interface_t* rtInterf
+)
+{
+    (void)size;
+    (void)options;
+
+    cjnative_interface_instance             = *rtInterf;
+    interpInterf->version                   = 1;
+    interpInterf->fiber_specific_data_size  = sizeof(Interpretation::Ectype);
+    interpInterf->iterator_size             = 0; // FIXME: remove
+    interpInterf->c2iVirtualExecutorAddr    = reinterpret_cast<uintptr_t>(&engine_c2i_call_pc_start);
+    interpInterf->c2iVirtualExecutorEndAddr = reinterpret_cast<uintptr_t>(&engine_c2i_call_pc_end);
+    interpInterf->fiber_destroy             = &FiberDestroy;
+    interpInterf->fiber_start               = &FiberStart;
+}
+
+CBC_EXPORT void interpreter_bridge_init(
+    size_t size,
+    char const** options,
+    struct interpreter_interface_t* interpInterf,
+    struct cjnative_interface_t* rtInterf
+)
+{
+    engine_runtime_bridge_initialize(size, options, interpInterf, rtInterf);
+}
 
 } // extern "C"
