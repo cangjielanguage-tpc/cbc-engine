03f222107963a1fb2074e5c8b398ac75954c5a9b
wellox <zapanov.rinchin@huawei.com>
2026-03-19 11:38:16 +0000

    feat: add docs

--- src/cbc/dispatcher_rt.h
+++ src/cbc/dispatcher_rt.h
@@ -18,6 +18,27 @@ struct Thunk {
     void* arg;
 };
 
+/// The interpretation loop can be used in two scenarious:
+/// - (main scenario) as interpreter for real runtime;
+/// - as part of unit test framework;
+///
+/// To share the code, an actual RuntimeInterface that is being used from interpreter is injected
+/// as template parameter.
+///
+/// For the main scenario the layout of the stack MUST avoid non-leaf C++ frames
+/// to handle fiber stack expansion properly.
+/// To achieve that, any compiled code invocation (and similar calls) is called not from
+/// interpreter loop (this function) itself, but from assembly-written function `perform_2i_call`
+/// that strictly tracks the frame layout.
+///
+/// So, to perform an invocation from `perform_2i_call` we are returning by value a `Thunk`,
+/// that contains both function to call and one argument.
+///
+/// We are restricting an number of arguments in `Thunk` structure to 1, to allow passing the structure
+/// by value on registers on System-V x64 or AArch64 ABIs.
+///
+/// Note, that an actual calling convention of `thunk.function`
+/// is different from ASM in unit-test framework.
 template <typename RTI>
 Thunk InterpretationLoop(
     Ectype* ectype, Frame* frame, ThreadHandle handle, LiteralTable* literals, Decoder::ByteReader& reader0
