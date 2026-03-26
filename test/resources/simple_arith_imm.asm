68f1ea88a0f47297aa597c164f6f8a960f7f146d
kees <>
2026-03-26 14:59:02 +0700

    upd

--- test/resources/simple_arith_imm.asm
+++ test/resources/simple_arith_imm.asm
@@ -4,7 +4,7 @@
     mov.64 IR1, 0x0
     mov.64 IR2, 0x7
     addi.64 IR2, IR2, 0x14
-    xori.64 IR1, IR3, 0x7
+    xori.64 IR1, IR3, 0x5
     div.64 IR3, IR2, IR1
     subi.64 IR1, IR3, 0x4
     ret.64 IR1
