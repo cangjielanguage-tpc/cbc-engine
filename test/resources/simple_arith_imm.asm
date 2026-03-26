7e37ae39279e4f2f3b1d47179c97771ecb315d20
kees <>
2026-03-26 15:01:03 +0700

    upd

--- test/resources/simple_arith_imm.asm
+++ test/resources/simple_arith_imm.asm
@@ -1,10 +1,9 @@
 @type default {
   @method default.main()V {
-    mov.64 IR3, 0x1
-    mov.64 IR1, 0x0
-    mov.64 IR2, 0x7
+    mov.64 IR2, 0x5
+    mov.64 IR3, 0x7
     addi.64 IR2, IR2, 0x14
-    xori.64 IR1, IR3, 0x5
+    xori.64 IR1, IR3, 0x2
     div.64 IR3, IR2, IR1
     subi.64 IR1, IR3, 0x4
     ret.64 IR1
