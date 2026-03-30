d9b38bd0fdb1fc3c3992b9a5034980c7fa552168
kees <>
2026-03-30 15:38:55 +0700

    fix: fix BccImm decoding

--- src/cbc/parser.cpp
+++ src/cbc/parser.cpp
@@ -362,11 +362,12 @@ template <> void Parser::Decode<InputOpcode::Bcc>()
 
 template <> void Parser::Decode<InputOpcode::BccImm>()
 {
-    auto [cc, w, l, imm64, target] = Decoder::ByteReaderM(codeReader)
+    auto [cc, w, z, l, imm64, target] = Decoder::ByteReaderM(codeReader)
                                          .Read4<InputCcOpc>()
                                          .Read4<Width::Value>()
+                                         .Read4()
                                          .Read4<IReg::Value>()
-                                         .Read4<uint64_t>()
+                                         .Read64<uint64_t>()
                                          .Read32<int32_t>()
                                          .Get();
     DoBranchIfImm(cc, w, l, imm64, codeReader.Cursor() + target);
