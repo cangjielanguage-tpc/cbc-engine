fdb330085e545f01f04ddcac2299b18be5ca7702
kees <>
2026-04-30 16:06:10 +0700

    feat: test float ops via gtest parameters

--- test/resources/simple_arith_float/simple_arith_float64.asm
+++ test/resources/simple_arith_float/simple_arith_float64.asm
@@ -1,4 +1,19 @@
 @type default {
+  @method default test_NEG [ ] F64 {
+    fneg.64 FR0, FR0
+    fret.64 FR0
+  }
+
+  @method default test_SQRT [ ] F64 {
+    fsqrt.64 FR0, FR0
+    fret.64 FR0
+  }
+
+  @method default test_ABS [ ] F64 {
+    fabs.64 FR0, FR0
+    fret.64 FR0
+  }
+
   @method default test_ADD [ ] F64 {
     fadd.64 FR0, FR0, FR1
     fret.64 FR0
