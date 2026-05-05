0280205ba25e39c627fbe0ca395572b5aa6219c8
kees <>
2026-05-05 19:35:08 +0700

    feat: add options + option parsing from cstr

---
+++ test/options_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "resolution/resolution.h"
+#include "cbc/isa_disasm.h"
+#include "runtimesupport/impl/entrypoint.h"
+
+#include "utils/logger.h"
+#include "utils/options.h"
+
+TEST(Options, fromCStr)
+{
+    const char* options[] = {
+        "cbc.log.resolution=trace",
+        "cbc.log.int=info",
+        "cbc.dasm=true",
+        "cbc.path=/path/to/cbc/sources"
+    };
+    constexpr size_t optionsCount = sizeof(options) / sizeof(decltype(options[0]));
+
+    Options::ParseAndSetOptions(optionsCount, options);
+
+    ASSERT(Resolution::log.GetLogLevel() == Logging::Level::TRACE);
+    ASSERT(Cbc::g_IsRawDisasmEnabled);
+    ASSERT(!g_cbcPath.compare("/path/to/cbc/sources"));
+}
