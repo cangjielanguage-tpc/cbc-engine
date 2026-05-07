13fd8e3efde9a6424f3c85c79be1542073d9d4f2
Vova Zhakulin <zhakulin.vladimir@h-partners.com>
2026-05-07 18:43:39 +0700

    wip: before rebase

--- tools/dis/env.h
+++ tools/dis/env.h
@@ -13,14 +13,5 @@ struct Option {
 
 #define OPTIONS_AMOUNT 2
 
-inline BoolOption resolve = BoolOption { "r", "resolve", false };
-
-inline ValueOption cbcPath = ValueOption { "cp", "cbc", "" };
-
-using Callback = std::function<void(Option&, std::string)>;
-
-inline std::unordered_map<std::string, Option&, Callback> options = { { resolve.shortName, resolve },
-                                                                      { cbcPath.shortName, cbcPath } };
-
 } // namespace Env
 } // namespace Dis
--- tools/dis/main.cpp
+++ tools/dis/main.cpp
@@ -1,15 +1,47 @@
 #include "cli.h"
-// #include "engine/symlevel/cbc_file.h"
-// #include "engine/symlevel/io/filesystem.h"
-// #include "engine/symlevel/terms.h"
+#include "engine/engine.h"
+#include "engine/symlevel/cbc_file.h"
+#include "engine/symlevel/io/filesystem.h"
+#include "engine/symlevel/terms.h"
 #include <filesystem>
-#include <iostream>
+#include <memory>
+#include <string_view>
+
+namespace Dis {
+
+class Disasmer {
+    std::unique_ptr<Engine::Session> sessionFor(std::vector<std::string_view> views)
+    {
+        auto loader = Engine::Loader();
+        for (auto view : views) {
+            auto raf = IO::OpenFile(std::filesystem::path(view));
+            loader.Load(std::move(raf), view);
+        }
+        std::unique_ptr<Engine::Session> s { new Engine::Session(loader.Build()) };
+        return s;
+    }
+
+    void DisasmOf(Symlevel::CbcFile& file) { file }
+
+    std::unique_ptr<Engine::Session> session;
+    std::vector<Symlevel::CbcFile>& files;
+
+public:
+    void Disasm()
+    {
+        for (auto& file : files) {
+            DisasmOf(file);
+        }
+    }
+
+    Disasmer(std::vector<std::string_view> views) : session(sessionFor(views)), files(session->GetEngine().files()) {}
+};
+} // namespace Dis
 
 int main(int argc, char* argv[])
 {
-    auto opts = Dis::Cli::CliOptions(argc, argv);
-    // std::string name = "simple.asm";
-    // auto raf         = IO::OpenFile(std::filesystem::path(name));
-    // auto cbc         = Symlevel::CbcFile::Create(IO::FileId(0), *raf, name);
-    // std::cout << cbc.GetPath() << std::endl;
+    // TODO: parse options etc.
+    std::string name = argv[1];
+    auto disasmer    = Dis::Disasmer({ name });
+    disasmer.Disasm();
 }
