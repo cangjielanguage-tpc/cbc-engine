6b3342a7ad3b89f154ed9252c54b2f0024b45fff
wellox <zapanov.rinchin@huawei.com>
2026-05-07 13:49:20 +0000

    fix: method table logging

--- src/runtimesupport/impl/typeinfo_factory.cpp
+++ src/runtimesupport/impl/typeinfo_factory.cpp
@@ -236,7 +236,7 @@ static std::optional<TypeInfo> CreateTypeInfoDyn(
 
         Log::typeinfo.Log(Logging::Level::INFO, [&](Stream::Output& out) {
             Stream::ResolvingOutput stream(session, out);
-            stream << term << *mt << Stream::endl;
+            stream << term << " " << *mt << Stream::endl;
         });
 
         auto extDefCount       = mt->ClassCount() + mt->InterfaceCount();
