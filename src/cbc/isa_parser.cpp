ca2bb72b2440a1abb9d94bce3f41012b88b17117
wellox <zapanov.rinchin@huawei.com>
2026-05-22 07:20:40 +0000

    fix: ldstackrec parse

--- src/cbc/isa_parser.cpp
+++ src/cbc/isa_parser.cpp
@@ -393,7 +393,7 @@ struct IsaParserImpl {
 
     static void LoadStackRec(IsaParser& parser)
     {
-        auto [r, ts] = ByteReaderM(parser.reader).ReadU4Skip4().ReadU16().Get();
+        auto [skip, r, ts] = ByteReaderM(parser.reader).ReadU4().ReadU4().ReadU16().Get();
         parser.LoadStackRec(r, ts);
     }
 
