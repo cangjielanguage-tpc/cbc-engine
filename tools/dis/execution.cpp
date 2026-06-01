8cc6bba2aadde10f171e913bf499c3b343cdcfb8
e00857587 <enes.denis1@huawei.com>
2026-06-01 07:30:58 +0000

    fix

--- tools/dis/execution.cpp
+++ tools/dis/execution.cpp
@@ -66,6 +66,8 @@ uint32_t MetaInfo::GetTypeSize(TypeInfo ti) { return 0; }
 
 uint8_t MetaInfo::GetAlign(TypeInfo ti) { return alignof(max_align_t); }
 
+void MetaInfo::VisitReferences(TypeInfo ti, std::function<void(uint32_t)> visitor) { FATAL("Should not be called"); }
+
 TypeInfo MetaInfo::ByteArrayTypeInfo() { return TypeInfo(nullptr); }
 
 } // namespace RTSupport
