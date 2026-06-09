3075da9ba1f7889104aede23433b57cd36234571
Ivan Trepakov <trepakov.ivan@huawei.com>
2026-06-09 15:55:49 +0700

    fix: fix frame start log

--- src/interpreter/interpretation_loop.cpp
+++ src/interpreter/interpretation_loop.cpp
@@ -72,7 +72,7 @@ Interpretation::Thunk engine_interpretation_loop(
     // [int] (stack depth) < (bc pos): instruction
     #define LOG_INSTR                                                                                                  \
         do {                                                                                                           \
-            logger.PrintFmt("#0x%x < 0x%03lx: ", (uint32_t)frame.start, pos - start);                                  \
+            logger.PrintFmt("#0x%lx < 0x%03lx: ", frame.start, pos - start);                                  \
             pos = reader.Cursor();                                                                                     \
             Cbc::RT::Log(literals, logger, args);                                                                      \
         } while (0)
