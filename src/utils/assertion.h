d5a68619111ad23a9919341982df2e63b572590a
Sharapov-Maksim <sharapov.maxim1@huawei.com>
2026-06-23 18:56:30 +0700

    ios: log assertion

--- src/utils/assertion.h
+++ src/utils/assertion.h
@@ -39,16 +39,45 @@
     #include <stdarg.h>
     #include <stdio.h>
 
+    #if defined(__APPLE__) && __has_include(<TargetConditionals.h>)
+        #include <TargetConditionals.h>
+    #endif
+
+    #if defined(TARGET_OS_IOS) && TARGET_OS_IOS && __has_include(<os/log.h>)
+        #include <os/log.h>
+        #define CBC_ENGINE_IOS_OS_LOG 1
+    #else
+        #define CBC_ENGINE_IOS_OS_LOG 0
+    #endif
+
 [[noreturn]]
 static void ReportFailure(const char* filename, int line, const char* func, const char* fmt, ...)
 {
     fprintf(stderr, "%s:%d: assertion failed in %s: ", filename, line, func);
     va_list args;
     va_start(args, fmt);
+    #if CBC_ENGINE_IOS_OS_LOG
+    va_list logArgs;
+    va_copy(logArgs, args);
+    #endif
     vfprintf(stderr, fmt, args);
     va_end(args);
     fprintf(stderr, "\n");
     fflush(stderr);
+
+    #if CBC_ENGINE_IOS_OS_LOG
+    char message[4096];
+    int written = vsnprintf(message, sizeof(message), fmt, logArgs);
+    va_end(logArgs);
+    if (written < 0) {
+        snprintf(message, sizeof(message), "<failed to format assertion message>");
+    }
+
+    os_log_error(
+        OS_LOG_DEFAULT, "%{public}s:%d: assertion failed in %{public}s: %{public}s", filename, line, func, message
+    );
+    #endif
+
     ASSERTION_TRAP();
 }
 
