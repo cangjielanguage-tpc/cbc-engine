f018d22d4f7f5c349e62f28336a7b7317b4dc95f
wellox <zapanov.rinchin@huawei.com>
2026-06-30 13:13:50 +0000

    feat: ascii graphics of frame layout and better macros

--- src/interpreter/ectype.h
+++ src/interpreter/ectype.h
@@ -108,6 +108,7 @@ class EctypeInvariants {
     static_assert(offsetof(Ectype, iregs) == ECTYPE_IREGS_OFFSET);
     static_assert(offsetof(Ectype, fregs) == ECTYPE_FREGS_OFFSET);
     static_assert(offsetof(Ectype, iregs[IReg::IR_ACC]) == ECTYPE_IACC_OFFSET);
+    static_assert(ECTYPE_IACC_NUM == IReg::IR_ACC);
     static_assert(IReg::COUNT == ECTYPE_IREGS_COUNT);
 };
 
