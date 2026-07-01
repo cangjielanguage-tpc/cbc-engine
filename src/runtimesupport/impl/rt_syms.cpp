0cbc2026762333df555cb6fbc69474ee5575b1bb
e00857587 <enes.denis1@huawei.com>
2026-07-01 13:37:00 +0000

    fix: do not close handle of helper lib

--- src/runtimesupport/impl/rt_syms.cpp
+++ src/runtimesupport/impl/rt_syms.cpp
@@ -19,11 +19,24 @@ struct Handle {
     void* handle;
     std::string name;
 
+    Handle() : handle(nullptr) {}
+
     Handle(void* handle, std::string&& name) : handle(handle), name(std::move(name)) {}
 
     Handle(Handle const& handle) = delete;
 
     Handle(Handle&& handle) : handle(handle.handle) { handle.handle = nullptr; }
+
+    Handle& operator=(Handle&& other)
+    {
+        if (handle != nullptr) {
+            FATAL("Trying to rewrite existing handle");
+        }
+        name         = std::move(other.name);
+        handle       = other.handle;
+        other.handle = nullptr;
+        return *this;
+    }
 
     static std::optional<Handle> Open(std::string&& str)
     {
@@ -53,6 +66,9 @@ struct Handle {
         }
     }
 };
+
+// Prolongs lifetime of helper lib handle after finish of Initialize.
+Handle g_helperLibHandle;
 
 void Initialize(DYN_CJNativeInterface* interf)
 {
@@ -100,6 +116,7 @@ void Initialize(DYN_CJNativeInterface* interf)
         return;
     }
 
+    g_helperLibHandle                      = std::move(helperHandleOpt.value());
     Asm::engine_implicit_exception_thrower = reinterpret_cast<void (*)(int)>(throwerSym);
 }
 
