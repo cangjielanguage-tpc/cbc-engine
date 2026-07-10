8468bd1553e71182ab8400cb42019401d160b5b4
e00857587 <enes.denis1@huawei.com>
2026-07-10 13:29:05 +0000

    fix: simplify pair printing

--- src/utils/ostream.h
+++ src/utils/ostream.h
@@ -63,11 +63,7 @@ public:
 
     template <typename T1, typename T2> void Print(const std::pair<T1, T2> pair)
     {
-        PrintFmt("(");
-        Print(pair.first);
-        PrintFmt(", ");
-        Print(pair.second);
-        PrintFmt(")");
+        *this << "(" << pair.first << ", " << pair.second << ")";
     }
 };
 
