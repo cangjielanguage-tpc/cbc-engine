6adf828298cab69418d3e49af4fc82c67161a93a
ilyas <selimov.ilyas1@huawei.com>
2026-07-14 18:12:06 +0700

    remove todos

--- src/cbc/isa_parser.cpp
+++ src/cbc/isa_parser.cpp
@@ -747,7 +747,6 @@ struct IsaParserImpl {
         parser.AtomicStore(src, sak, obj, field);
     }
 
-    // TODO deduplicate with AtomicSwap
     static void CAS(IsaParser& parser)
     {
         auto [width, dst, obj, src1, src2, field] = ByteReaderM(parser.reader).ReadU8().ReadU4().ReadU4().ReadU4().ReadU4().ReadU16().Get();
@@ -762,7 +761,6 @@ struct IsaParserImpl {
         }
     }
 
-    // TODO deduplicate with CAS
     static void AtomicSwap(IsaParser& parser)
     {
         auto [width, dst, obj, src, field] = ByteReaderM(parser.reader).ReadU8().ReadU4().ReadU4().ReadU4Skip4().ReadU16().Get();
