c5eb283cd09e19bbe34b371682b4d1ca68f4123c
nreshetnikova <reshetnikova.natalia@h-partners.com>
2026-07-30 16:05:59 +0700

    varray: fix getName

--- src/engine/terms.cpp
+++ src/engine/terms.cpp
@@ -373,7 +373,7 @@ void Term::GetName(Session& session, Stream::Output& out, bool hasDebugPrefix) c
         }
 
         case TK::VARRAY: {
-            stream << "VArray<" << Subterm(0) << ", $" << Subterm(1) << '>';
+            stream << "VArray<" << Subterm(0) << ", $" << static_cast<VArrayTermId>(GetId()).GetNum() << '>';
             break;
         }
 
