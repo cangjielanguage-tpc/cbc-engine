067063aa07cde443cc0f8659488531a7b5ca83e2
Sharapov Maksim <sharapov.maxim1@huawei.com>
2026-07-31 19:31:25 +0700

    extend stack reservation

--- src/asm_export.h
+++ src/asm_export.h
@@ -36,4 +36,9 @@
 
 #define TYPEINFO_DATA_MT_OFFSET 96
 
-#define ADDITIONAL_STACK_SPACE 8 * 1024
+// TODO: adjust values appropriately (it is best if the ADDITIONAL_STACK_SPACE is 0 for release mode)
+#ifndef NDEBUG
+    #define ADDITIONAL_STACK_SPACE (32 * 1024)
+#else
+    #define ADDITIONAL_STACK_SPACE (4 * 1024)
+#endif // NDEBUG
