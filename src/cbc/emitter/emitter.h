59d31db2b5062f432aa9c23b37f10fda8e758d20
wellox <zapanov.rinchin@huawei.com>
2026-08-04 11:37:41 +0000

    feat: spawn future support
    (cherry picked from commit f449b1823816c5fd190fa944dbced38eb555bb97)

--- src/cbc/emitter/emitter.h
+++ src/cbc/emitter/emitter.h
@@ -203,10 +203,11 @@ public:
 
     void InitClosure(bool instantiatedSret);
     void Spawn(RTSupport::TypeInfo typeInfo);
+    void SpawnFuture();
 
     void NewObjGeneric(IReg ti);
     void NewObjGenericOnAcc(IReg ti);
-    void NewObj(RTSupport::TypeInfo typeInfo);
+    void NewObj(RTSupport::TypeInfo typeInfo, bool pinned);
     void NewArr(RTSupport::TypeInfo typeInfo);
     void LoadObj(LoadAccessKind ldk, Reg dst, IReg base, uint32_t offset);
     void StoreObj(StoreAccessKind stk, Reg src, IReg base, uint32_t offset);
--- src/cbc/isa_rewriter.cpp
+++ src/cbc/isa_rewriter.cpp
@@ -46,6 +46,7 @@ using STK = Format::StoreAccessKind;
 
 enum class New {
     Obj,
+    ObjPinned,
     Arr,
 };
 
@@ -926,10 +927,19 @@ struct IsaRewriter : public IsaParser {
             return std::nullopt;
         }
 
+        if (kind == New::Obj && t->term.GetKind() == Engine::TermKind::TYPE) {
+            auto identifier = Engine::ExtractTypeDefIdentifier(t->term);
+            auto def        = Symlevel::Reader::Read(session, identifier);
+            if (Symlevel::Reader::Read(session, def.GetName()).compare("std.core:Future") == 0) {
+                kind = New::ObjPinned;
+            }
+        }
+
         auto typeInfo = *ti;
         switch (kind) {
-            case New::Obj: emit.NewObj(typeInfo); break;
-            case New::Arr: emit.NewArr(typeInfo); break;
+            case New::Obj:       emit.NewObj(typeInfo, false); break;
+            case New::Arr:       emit.NewArr(typeInfo); break;
+            case New::ObjPinned: emit.NewObj(typeInfo, true); break;
         }
         BindStatePoint();
         AdjustReg(dst, IReg::IR1);
@@ -1014,7 +1024,7 @@ struct IsaRewriter : public IsaParser {
 
     void Spawn(IReg closure, uint32_t typeId) override
     {
-        AdjustReg(IReg::IR1, closure);
+        AdjustReg(IReg::IR_ACC, closure);
 
         auto t = resolver.QueryFutureByFunctional(Index<Type>(typeId));
         if (!t.has_value()) {
@@ -1032,10 +1042,10 @@ struct IsaRewriter : public IsaParser {
         BindStatePoint();
     }
 
-    void SpawnFuture(IReg future, uint32_t type) override
+    void SpawnFuture(IReg future, uint16_t typeId) override
     {
-        BindStatePoint();
-        FATAL("not implemented");
+        AdjustReg(IReg::IR_ACC, future);
+        emit.SpawnFuture();
     }
 
     void CallClosure(IReg dst, uint32_t typeId, bool generic) override
