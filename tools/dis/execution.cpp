4390c65a7f4943526132a0a3778ae3fd4abd13cc
wellox <zapanov.rinchin@huawei.com>
2026-08-04 11:37:41 +0000

    feat: spawn future support

--- tools/dis/execution.cpp
+++ tools/dis/execution.cpp
@@ -90,6 +90,8 @@ Interpretation::Thunk Execution::GetInterfaceThunk(TypeInfo where, TypeInfo ti, 
 
 void* Execution::AllocateObjectInstance() { FATAL("Should not reach here"); }
 
+void* Execution::AllocateObjectPinnedInstance() { FATAL("Should not reach here"); }
+
 void* Execution::AllocateObjectInstanceAcc() { FATAL("Should not reach here"); }
 
 void* Execution::AllocateArrayInstance() { FATAL("Should not reach here"); }
@@ -111,6 +113,8 @@ void* Execution::ThrowImplicitException()
 void* Execution::GcPointTrampoline() { FATAL("Should not reach here"); }
 
 void* Execution::Spawn() { FATAL("Should not reach here"); }
+
+void* Execution::SpawnFuture() { FATAL("Should not reach here"); }
 
 void* Execution::GcPoint() { FATAL("Should not reach here"); }
 
