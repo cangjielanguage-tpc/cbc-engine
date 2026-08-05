0b2638a335d44b755739c6c01d87d613fc120ac2
Sharapov-Maksim <sharapov.maxim1@huawei.com>
2026-08-05 12:40:36 +0700

    fix SpawnFuture support

--- tools/launcher/cbcengine-helper.c
+++ tools/launcher/cbcengine-helper.c
@@ -8,22 +8,20 @@ struct StackReturn {
 
 extern uintptr_t CJ_MCC_ReadRefField(void* object, uintptr_t* field);
 extern void* CJ_MCC_NewCJThread(void* execute, void* future, void* scheduler);
-extern struct StackReturn set_thread_handle(void* thread, void* handle, void* zero) __asm__(
-    "_CNat6Thread24setRuntimeCJThreadHandleHPu"
-);
-extern void execute() __asm__("_CNat6FutureIG_E7executeHv");
+extern struct StackReturn _CNat6Thread24setRuntimeCJThreadHandleHPu(void* thread, void* handle, void* zero);
+extern void _CNat6FutureIG_E7executeHv();
 
 struct Future {
     void* header;
     uintptr_t thread;
 };
 
-void helper_spawn_future(struct Future* future)
+__attribute__((used)) void helper_spawn_future(struct Future* future)
 {
-    void* handle     = CJ_MCC_NewCJThread((void*)&execute, future, NULL);
+    void* handle     = CJ_MCC_NewCJThread((void*)&_CNat6FutureIG_E7executeHv, future, NULL);
     uintptr_t thread = future->thread;
     if ((thread >> 48 != 0)) {
         thread = CJ_MCC_ReadRefField(future, &future->thread);
     }
-    set_thread_handle((void*)thread, handle, NULL);
+    _CNat6Thread24setRuntimeCJThreadHandleHPu((void*)thread, handle, NULL);
 }
