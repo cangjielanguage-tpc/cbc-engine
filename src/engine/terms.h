cc8eb6cce15510b25bb46030c646d5dc1a976e5d
wellox <zapanov.rinchin@huawei.com>
2026-08-13 10:24:20 +0000

    fix: limits for refid/offset/file id

--- src/engine/terms.h
+++ src/engine/terms.h
@@ -107,7 +107,7 @@ public:
 protected:
     constexpr TermId(TermKind kind, uint64_t info) : kind(kind), info(info)
     {
-        ASSERT(info < (1lu << INFO_PART_BIT_SIZE));
+        ASSERT(info < (1llu << INFO_PART_BIT_SIZE));
     }
 
     uint64_t Raw() const { return Bits::Raw64(*this); }
