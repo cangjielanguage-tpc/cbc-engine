e8bcf80c9cd07b1ddfd2b49e2db4aca3e006df1f
wellox <zapanov.rinchin@huawei.com>
2026-08-14 08:38:54 +0000

    wip: remove parsing from model entities

--- src/engine/symlevel/reader.h
+++ src/engine/symlevel/reader.h
@@ -3,7 +3,6 @@
 #include "engine/engine.h"
 #include "io/file_id.h"
 #include "offset.h"
-#include "string.h"
 
 namespace Symlevel {
 
@@ -12,11 +11,5 @@ public:
     template <typename T> static T Read(Engine::Session& session, IO::FileId fileId, Offset<T> offset);
     template <typename T> static T Read(Engine::Session& session, Engine::Identifier<T> id);
 };
-
-template <> Symlevel::String Reader::Read<Symlevel::String>(Engine::Session& session, Engine::Identifier<String> id);
-template <>
-Symlevel::String Reader::Read<Symlevel::String>(
-    Engine::Session& session, IO::FileId fileId, Offset<Symlevel::String> offset
-);
 
 } // namespace Symlevel
--- src/engine/symlevel/string.h
+++ src/engine/symlevel/string.h
@@ -8,9 +8,6 @@ namespace Symlevel {
 
 class String : public std::string_view {
 public:
-    // static String Parse(Engine::Session& session, IO::FileId fileId, Offset<String> offset);
-    // static String Parse(Engine::Session& session, Engine::Identifier<String> ident);
-
     String(std::string_view view) : std::string_view(std::move(view)) {}
 };
 
