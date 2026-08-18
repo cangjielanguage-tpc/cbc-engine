a9e169ff02d02d7c5eed897d6939ef9b553fed9f
wellox <zapanov.rinchin@huawei.com>
2026-08-18 13:29:30 +0700

    fix: copy to obj

--- src/cbc/isa_rewriter.cpp
+++ src/cbc/isa_rewriter.cpp
@@ -1578,7 +1578,7 @@ struct IsaRewriter : public IsaParser {
             case HEAD_STATIC:
                 // IRZ means static record field, so whole position is encoded in accumulated offset
                 // FIXME: encode as separate operation
-                msr.emit.CopyRecToObj(from, msr.base, *ty.GetTypeInfo());
+                msr.emit.CopyRecToObj(from, IReg::IRZ, *ty.GetTypeInfo());
                 break;
                 break;
             case HEAD_FRAME:
