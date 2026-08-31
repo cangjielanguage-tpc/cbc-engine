5bb0d98e72adfbcb681513244bf80e90166540fa
Ivan Trepakov <trepakov.ivan@huawei.com>
2026-08-31 14:16:30 +0700

    feat: implement array length

--- src/cbc/isa_rewriter.cpp
+++ src/cbc/isa_rewriter.cpp
@@ -1193,7 +1193,9 @@ struct IsaRewriter : public IsaParser {
         emit.StringLit(storage, frameLayout.typedOffset.at(ts));
     }
 
-    void ArrayLength(IReg dst, IReg arr) override { FATAL("not implemented"); }
+    void ArrayLength(IReg dst, IReg arr) override {
+        emit.LoadObj(Format::LoadAccessKind::LD_64, dst, arr, RTSupport::MetaInfo::ObjectHeaderSize());
+    }
 
     void ArrayIndexCheck(IReg length, IReg index) override { FATAL("not implemented"); }
 
