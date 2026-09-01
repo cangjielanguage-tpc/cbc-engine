82fae12d88502197003cbace57d8a3a373b39bf9
e00857587 <enes.denis1@huawei.com>
2026-09-01 12:20:56 +0000

    feat: parse and disasm new ld/st/lea instructions

--- src/cbc/isa_rewriter.cpp
+++ src/cbc/isa_rewriter.cpp
@@ -393,6 +393,12 @@ struct IsaRewriter : public IsaParser {
         emit.LoadStatic(Ldk(field->fieldType.GetKind()), dst, symbol);
     }
 
+    void LdTyped(AnyReg dst, uint16_t slot, uint32_t field) override { FATAL("TODO: support"); }
+
+    void LdDerived(AnyReg dst, IReg baseRef, IReg derived, uint32_t field) override { FATAL("TODO: support"); }
+
+    void LdGeneric(AnyReg dst, IReg baseRef, IReg derived, IReg ti, uint32_t field) override { FATAL("TODO: support"); }
+
     void Lea(IReg dst, IReg base, uint32_t fieldId) override
     {
         UNWRAP_OPT(field, resolver.Query(Index<InstanceField>(fieldId)), Fail);
@@ -406,6 +412,10 @@ struct IsaRewriter : public IsaParser {
             emit.LoadObj(LDK::LD_LEA, dst, base, offset);
         }
     }
+
+    void LeaStatic(IReg dst, IReg dstBaseRef, uint32_t field) override { FATAL("TODO: support"); }
+
+    void LeaGeneric(IReg dst, IReg base, IReg ti, uint32_t field) override { FATAL("TODO: support"); }
 
     void St(AnyReg src, IReg base, uint32_t fieldId) override
     {
@@ -429,6 +439,12 @@ struct IsaRewriter : public IsaParser {
         auto symbol = emit.NewAddressSym(location);
         emit.StoreStatic(Stk(field->fieldType.GetKind()), src, symbol);
     }
+
+    void StTyped(AnyReg src, uint16_t slot, uint32_t field) override { FATAL("TODO: support"); }
+
+    void StDerived(AnyReg src, IReg baseRef, IReg derived, uint32_t field) override { FATAL("TODO: support"); }
+
+    void StGeneric(AnyReg src, IReg baseRef, IReg derived, IReg ti, uint32_t field) override { FATAL("TODO: support"); }
 
     void LeaBox(IReg dst, IReg base) override
     {
