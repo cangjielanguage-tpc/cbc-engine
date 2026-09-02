db1e3ff0cadba4d849b9cd3c9b1bcdaaf77a5a1f
e00857587 <enes.denis1@huawei.com>
2026-09-02 09:43:29 +0000

    fix: unwrap opt in release mode

--- src/utils/misc.h
+++ src/utils/misc.h
@@ -5,6 +5,14 @@
 #include <string>
 #include <vector>
 
+#define UNWRAP_OPT(name, expression, handler)                                                                          \
+    auto __##name = (expression);                                                                                      \
+    if (!__##name.has_value()) {                                                                                       \
+        handler();                                                                                                     \
+        return;                                                                                                        \
+    }                                                                                                                  \
+    auto name = __##name.value();
+
 namespace Std {
 namespace Vector {
 
