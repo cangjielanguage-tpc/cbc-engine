03ffde20574275033bd1b5b91cfd1a93e4454994
nreshetnikova <reshetnikova.natalia@h-partners.com>
2026-09-04 12:15:40 +0700

    memops: fix

--- src/interpreter/interpretation_loop.cpp
+++ src/interpreter/interpretation_loop.cpp
@@ -747,7 +747,7 @@ LABEL(INDEX) {
     auto size = MetaInfo::GetTypeSize(args.ti);
     auto offset = RTSupport::MetaInfo::ArrayBodyOffset() + interpreter.MemOffsetReg(idx.IR()) * size;
     Log::interpretation.Stream(Logging::Level::INFO).PrintFmt("dst = %p, arr = %p, offset = %ld\n", ectype->GetReference(dst.IR()).value, obj.value, offset);
-    MemoryLocation(obj.value, offset).LoadRef(dst, ectype);
+    MemoryLocation(obj.value, offset).Lea(dst, ectype);
     NEXT;
 }
 
